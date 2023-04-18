cb76d440d6372d846478a8d85f38a6a330f188f2
aghosh03 <avikghosh03@gmail.com>
2023-04-17 22:37:43 -0700

    commit 2

--- app/settings.py
+++ app/settings.py
@@ -129,7 +129,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/static'),
 ]
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
